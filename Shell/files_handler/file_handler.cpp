306562e06d6a77c85200ba1108b2c52486a3a5e7
Khaled Abdelfattah <khaledabdelfattah74@gmail.com>
2018-10-04 19:43:49 +0200

    Adding loggers

---
+++ Shell/files_handler/file_handler.cpp
@@ -0,0 +1,14 @@
+//
+//  file_handler.cpp
+//  Shell
+//
+//  Created by Khaled Abdelfattah on 10/4/18.
+//  Copyright © 2018 Khaled Abdelfattah. All rights reserved.
+//
+
+#include "file_handler.hpp"
+
+void write_into_processes_logger() {
+    File* file_ptr = fopen("../processes_log.txt", "a");
+    if (file_ptr != NULL)
+}
---
+++ Shell/files_handler/file_handler.hpp
@@ -0,0 +1,19 @@
+//
+//  file_handler.hpp
+//  Shell
+//
+//  Created by Khaled Abdelfattah on 10/4/18.
+//  Copyright © 2018 Khaled Abdelfattah. All rights reserved.
+//
+
+#ifndef file_handler_hpp
+#define file_handler_hpp
+
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+
+void write_into_commands_logger();
+void write_into_processes_logger();
+
+#endif /* file_handler_hpp */
